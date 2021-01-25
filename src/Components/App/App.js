073b536ba885226eb86a0d5379201b4bdcd0dd4e
Ibrahim Bello <ibb4u2006@yahoo.com>
2021-01-25 01:27:19 +0100

    Created app component

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>so<span className="highlight">ng</span>ify</h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
