0768ddc883ec5a4623ef1afde2573fad9348eb1f
Ibrahim Bello <ibb4u2006@yahoo.com>
2021-01-25 02:03:11 +0100

    Added Tracklist to PlayList component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,9 +1,11 @@
 import './Playlist.css';
+import TrackList from "../TrackList/TrackList";
 
 function Playlist() {
     return (
         <div className="Playlist">
             <input value="New Playlist"/>
+            <TrackList />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     );
