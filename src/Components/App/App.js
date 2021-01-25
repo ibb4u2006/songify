d7f3255dc055b65550e0f7700e363de411632ef1
Ibrahim Bello <ibb4u2006@yahoo.com>
2021-01-25 01:36:00 +0100

    Created SeachBar Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import '../SearchBar/SearchBar';
+import SearchBar from "../SearchBar/SearchBar";
 
 function App() {
   return (
       <div>
         <h1>so<span className="highlight">ng</span>ify</h1>
         <div className="App">
+          <SearchBar />
           <div className="App-playlist">
           </div>
         </div>
