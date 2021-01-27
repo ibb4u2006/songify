6da0d2d1737a9d51d6fb472257c5d51d92e8d97f
Ibrahim Bello <ibb4u2006@yahoo.com>
2021-01-27 22:32:19 +0100

    Passed down search result

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -6,6 +6,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
+                    <h3>{ this.props.track.name }</h3>
+                    <p>{ this.props.track.artist } | { this.props.track.album }</p>
                 </div>
                 <button className="Track-action">+/-</button>
             </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
     render() {
+        const tracks = this.props.tracks;
         return (
             <div className="TrackList">
+                { tracks && tracks.map((track) => {
+                    return <Track key={track.id} track={track} />;
+                }) }
             </div>
         );
     }
